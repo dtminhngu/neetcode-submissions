e1a61cf30c7cfde4d53f0c776fd7d970f4b080c4
Dương Tuấn Minh <duongtuanminh07@gmail.com>
2026-08-05 22:51:53 +0700

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        int note[26] = {0};
+        for (int i = 0; i < s.size(); i++) note[s[i] - 'a']++;
+        for (int i = 0; i < t.size(); i++) note[t[i] - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            if (note[i] != 0) return false;
+        }
+        return true;
+    }
+};
